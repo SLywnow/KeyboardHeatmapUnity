518a889c3949c90ca323b48d4cbc7606d009aa8f
SLywnow <nicbor1998@gmail.com>
2025-04-23 23:30:07 +0300

    1.1

--- Assets/Sc/KB_Button.cs
+++ Assets/Sc/KB_Button.cs
@@ -8,6 +8,8 @@ public class KB_Button : MonoBehaviour
 	public KB_Main main;
    public string button;
    public ulong pressCount;
+	public float time;
+	public bool press;
 
 	Image img;
 
@@ -22,12 +24,33 @@ public class KB_Button : MonoBehaviour
 		img.color = main.col.Evaluate(0);
 	}
 
+	private void Update()
+	{
+		if (main.timerMode.isOn)
+		{
+			if (press)
+			{
+				time += Time.deltaTime;
+				if (main.maxtime< time)
+					main.maxtime = time;
+			}
+
+			if (main.maxtime > 0)
+				img.color = main.col.Evaluate((float)time / (float)main.maxtime);
+			else
+				img.color = main.col.Evaluate(0);
+		}
+	}
+
 	public void UpdateUI()
 	{
-		if (main.maxpress > 0)
-			img.color = main.col.Evaluate((float)pressCount / (float)main.maxpress);
-		else
-			img.color = main.col.Evaluate(0);
+		if (!main.timerMode.isOn)
+		{
+			if (main.maxpress > 0)
+				img.color = main.col.Evaluate((float)pressCount / (float)main.maxpress);
+			else
+				img.color = main.col.Evaluate(0);
+		}
 	}
 
 }
--- Assets/Sc/KB_Main.cs
+++ Assets/Sc/KB_Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityRawInput;
+using SLywnow;
 
 public class KB_Main : MonoBehaviour
 {
@@ -11,38 +12,86 @@ public class KB_Main : MonoBehaviour
 	public KB_Button mousewheel;
 	public Gradient col;
 	public ulong maxpress;
+	public float maxtime;
 
 	[Header("UI")]
 	public Toggle enableWheel;
 	public Toggle enableMouse;
+	public Toggle timerMode;
+
+	[Header("Saves")]
+	public KB_Save def;
+	public KB_Save loaded;
+
+	private void Awake()
+	{
+		if (FilesSet.CheckFile(Application.dataPath + "/save.dat",false))
+		{
+			loaded = JsonUtility.FromJson<KB_Save>(FilesSet.LoadStream(Application.dataPath + "/save.dat", false, false));
+			Load();
+		}
+		else
+		{
+			RestoreDefault();
+		}
+	}
 
 	void Start()
 	{
 		RawInput.Start();
 		RawInput.WorkInBackground = true;
 		RawInput.OnKeyDown += HandleKeyDown;
+		RawInput.OnKeyUp += HandleKeyUp;
+
+		timerMode.onValueChanged.AddListener((bool b) => Clear());
 	}
 
 	private void HandleKeyDown(RawKey key)
 	{
 		string k = key.ToString();
-		if (enableWheel.isOn && enableMouse.isOn && (k == "WheelUp" || k == "WheelDown"))
+
+		if (timerMode.isOn)
 		{
-			ProcessButton(mousewheel);
+			KB_Button b = buttons.Find(f => f.button.ToLower() == k.ToLower());
+
+			if (b != null)
+			{
+				b.press = true;
+			}
 		}
 		else
 		{
-			if (!enableMouse.isOn && (k == "LeftButton" || k== "RightButton" || k== "MiddleButton"))
+			if (enableWheel.isOn && enableMouse.isOn && (k == "WheelUp" || k == "WheelDown"))
 			{
-				return;
+				ProcessButton(mousewheel);
 			}
+			else
+			{
+				if (!enableMouse.isOn && (k == "LeftButton" || k == "RightButton" || k == "MiddleButton"))
+				{
+					return;
+				}
 
+				KB_Button b = buttons.Find(f => f.button.ToLower() == k.ToLower());
+
+				ProcessButton(b);
+			}
+		}
+		//Debug.Log(key.ToString());
+	}
+
+	private void HandleKeyUp(RawKey key)
+	{
+		string k = key.ToString();
+		if (timerMode.isOn)
+		{
 			KB_Button b = buttons.Find(f => f.button.ToLower() == k.ToLower());
 
-			ProcessButton(b);
+			if (b != null)
+			{
+				b.press = false;
+			}
 		}
-
-		//Debug.Log(key.ToString());
 	}
 
 	void ProcessButton(KB_Button b)
@@ -61,16 +110,73 @@ public class KB_Main : MonoBehaviour
 	public void Clear()
 	{
 		maxpress = 0;
+		maxtime = 0;
 		foreach (KB_Button bt in buttons)
 		{
 			bt.pressCount = 0;
+			bt.time = 0;
 			bt.UpdateUI();
 		}
+	}
+
+	public void SelectGrad()
+	{
+		GradientPicker.Create(col, "Select button's gradient", GradUpdate, GradDone);
+	}
+
+	void GradUpdate(Gradient g)
+	{
+
+	}
+
+	void GradDone(Gradient g)
+	{
+		col = g;
+
+		foreach (KB_Button bt in buttons)
+		{
+			bt.UpdateUI();
+		}
+	}
+
+	public void RestoreDefault()
+	{
+		loaded = def;
+		Load();
+	}
+	
+	void Load()
+	{
+		enableWheel.isOn = loaded.enableWheel;
+		enableMouse.isOn = loaded.enableMouse;
+		timerMode.isOn = loaded.timerMode;
+		col = loaded.col;
+	}
+
+	public void Save()
+	{
+		loaded = new KB_Save();
+		loaded.enableWheel = enableWheel.isOn;
+		loaded.enableMouse = enableMouse.isOn;
+		loaded.timerMode = timerMode.isOn;
+		loaded.col = col;
+
+		FilesSet.SaveStream(Application.dataPath + "/save.dat", JsonUtility.ToJson(loaded, true), false, false);
 	}
 
 	private void OnDisable()
 	{
 		RawInput.Stop();
 		RawInput.OnKeyDown -= HandleKeyDown;
+		RawInput.OnKeyUp -= HandleKeyUp;
 	}
+}
+
+[System.Serializable]
+public class KB_Save
+{
+	public bool enableWheel;
+	public bool enableMouse;
+	public bool timerMode;
+	public Gradient col;
 }
